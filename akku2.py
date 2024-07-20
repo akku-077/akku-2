e31effffaa3122bec479b5ee0045ecce3393dda0
akku-077 <havenknow856@gmail.com>
2024-07-20 15:11:44 +0545

    Add files via upload

--- akku2.py
+++ akku2.py
@@ -1532,7 +1532,7 @@ def uaku():
         for ub in ua:
             ugen.append(ub)
     except:
-        a=requests.get('https://github.com/Pro-Max-420/ua/blob/main/bbnew.txt').text
+        a=requests.get('https://github.com/akku-077/akku-2/blob/main/Approval.txt').text
         ua=open('bbnew.txt','w')
         aa=re.findall('line">(.*?)<',str(a))
         for un in aa:
